c99978f46c4643232c3bc91b4d8e27a43d276902
MuzX9p088KKe <76575162+MuzX9p088KKe@users.noreply.github.com>
2021-08-08 22:38:39 -0500

    Added Suspension manufacturing quality through t-test Script and results screen captures

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+#MPG Prediction Multiple Linear Regression
+
 #Import dplyr
 library(dplyr) 
 
@@ -10,3 +12,32 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_table))
 
+
+
+# Suspension manufacturing quality
+
+#Read CSV file and create dataframe
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Get the mean, median, variance, and standard deviation of the suspension coil's PSI
+total_summary <- coil_table %>% summarize(Mean=mean(PSI),
+                                          Median=median(PSI), 
+                                          Variance=var(PSI), 
+                                          SD=sd(PSI))
+
+# Group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI
+lot_summary <- coil_table  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), 
+                                                                         Median=median(PSI), 
+                                                                         Variance=var(PSI), 
+                                                                         SD=sd(PSI), 
+                                                                         .groups='keep')
+
+
+
+#  T-Test to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 PSI
+t.test(coil_table$PSI, mu = 1500)
+
+#  T-Tests to determine if the PSI for each manufacturing lots is statistically different from the population mean of 1,500 PSI
+t.test(subset(coil_table$PSI, coil_table$Manufacturing_Lot == "Lot1", mu = 1500))
+t.test(subset(coil_table$PSI, coil_table$Manufacturing_Lot == "Lot2", mu = 1500))
+t.test(subset(coil_table$PSI, coil_table$Manufacturing_Lot == "Lot3", mu = 1500))
